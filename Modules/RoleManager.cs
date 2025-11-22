25a9d797f511c190d90a38d45809a888f8c14e14
Adraxas Nazaron <dragonjunk45@gmail.com>
2025-11-21 23:20:31 -0800

    New module for managing Roles

---
+++ Modules/RoleManager.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.WebSocket;
+using DragonBot.Core;
+using DragonBot.Instance;
+using Nito.AsyncEx;
+
+namespace DragonBot.Modules
+{
+    [RegisterModule]
+    internal sealed class RoleManager : ModuleBase, IModule<RoleManager>
+    {
+        public static string Name { get; } = "Core:RoleManager";
+        public static RoleManager Create(Bot bot)
+        {
+            return new RoleManager(bot);
+        }
+        private RoleManager(Bot bot) : base(bot)
+        {
+        }
+        public async Task AddRole(IGuildUser User, IRole role)
+        {
+            await User.AddRoleAsync(role);
+        }
+        public async Task RemoveRole(IGuildUser User, IRole role)
+        {
+            await User.RemoveRoleAsync(role);
+        }
+        public async Task<bool> HasRole(ulong UserID, IRole role)
+        {
+            //var user = bot.Client.GetGuild(bot.BotConfig.GuildID).GetUser(UserID);
+            //return user.Roles.Contains(role);
+            return false;
+        }
+    }
+}
