248fff99f770027707f3323828fc3bd48fecc291
Adraxas Nazaron <dragonjunk45@gmail.com>
2026-01-15 02:14:00 -0800

    Moved RoleManager to instance and converted it from a module to a dependency

---
+++ Instance/RoleManager.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.WebSocket;
+using DragonBot.Core;
+using Nito.AsyncEx;
+
+namespace DragonBot.Instance
+{
+    public sealed class RoleManager
+    {
+        private readonly Bot bot;
+        private SocketGuild Guild { get; }
+        internal RoleManager(Bot bot)
+        {
+            this.bot = bot;
+            Guild = bot.Client.GetGuild(bot.BotConfig.GuildId);
+        }
+        public static async Task AddRole(IGuildUser User, IRole role)
+        {
+            await User.AddRoleAsync(role);
+        }
+        public static async Task RemoveRole(IGuildUser User, IRole role)
+        {
+            await User.RemoveRoleAsync(role);
+        }
+        public async Task<bool> HasRole(ulong UserId, IRole role)
+        {
+            var user = Guild.GetUser(UserId);
+            return user.Roles.Contains(role);
+        }
+        public async Task<string> IdToName(ulong roleId)
+        {
+            var role = bot.Client.GetGuild(bot.BotConfig.GuildId).GetRole(roleId);
+            return role.Name;
+        }
+        public async Task<ulong> NameToId(string roleName)
+        {
+            var role = bot.Client.GetGuild(bot.BotConfig.GuildId).Roles.FirstOrDefault(r => r.Name.Equals(roleName, StringComparison.OrdinalIgnoreCase));
+            return role?.Id ?? 0;
+        }
+    }
+}
