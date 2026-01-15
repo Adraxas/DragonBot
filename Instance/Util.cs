d75d7088a6552755af1593d185566d66b9193f36
Adraxas Nazaron <dragonjunk45@gmail.com>
2026-01-15 02:09:22 -0800

    Simplified IsCommandRegistered

--- Instance/Util.cs
+++ Instance/Util.cs
@@ -7,20 +7,14 @@ namespace DragonBot.Instance
     public class Util
     {
         private readonly Bot bot;
-        private List<Discord.WebSocket.SocketApplicationCommand> CommandCache { get; set; } = [];
         internal Util(Bot bot)
         {
             this.bot = bot;
-            RefreshCommandCache();
-        }
-        public void RefreshCommandCache()
-        {
-            CommandCache = bot.Client.GetGuild(bot.BotConfig.GuildId).GetApplicationCommandsAsync().Result.ToList();
         }
         public bool IsCommandRegistered(string commandName)
         {
-            RefreshCommandCache();
-            return CommandCache.Any(cmd => cmd.Name.Equals(commandName, StringComparison.OrdinalIgnoreCase));
+            return bot.Client.GetGuild(bot.BotConfig.GuildId).GetApplicationCommandsAsync().Result
+                .Any(cmd => cmd.Name.Equals(commandName, StringComparison.OrdinalIgnoreCase));
         }
 
         internal void InitializeModules(Bot bot)
