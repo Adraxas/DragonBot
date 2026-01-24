c9a1f16c761ef83a8d570660db3533660475be49
Adraxas Nazaron <dragonjunk45@gmail.com>
2026-01-23 22:21:07 -0800

    Refactored ModuleBase

--- Modules/ModuleBase.cs
+++ Modules/ModuleBase.cs
@@ -4,15 +4,11 @@ namespace DragonBot.Modules
 {
     public abstract class ModuleBase
     {
+        public List<string> Dependecies { get; } = [];
         protected readonly Bot bot;
         protected ModuleBase(Bot bot)
         {
             this.bot = bot;
-        }
-        public virtual void RegisterCommands()
-        {
-            // Override in derived classes to register commands
-            throw new NotImplementedException("Do not call base method");
         }
     }
 }
